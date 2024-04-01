cb383ec1d8c65e496c43c012b1b1c462c2ff9fef
Rudy Saal <rudyasaal3@gmail.com>
2024-03-31 23:58:26 -0400

    Premier soumission (non finit)

--- database.py
+++ database.py
@@ -11,12 +11,17 @@ class Database:
         """
             Chargez les variables d'environnement de votre fichier .env, puis complétez les lignes 15 à 19 afin de récupérer les valeurs de ces variables
         """
-
-        self.host =
-        self.port =
-        self.database =
-        self.user =
-        self.password =
+        load_dotenv("variables.env")
+        self.host = os.environ.get("HOST")
+        print(self.host)
+        self.port = int(os.environ.get("PORT"))
+        print(self.port)
+        self.database = os.environ.get("DATABASE")
+        self.user = os.environ.get("USER")
+        self.password = os.environ.get("PASSWORD")
+        print(self.database)
+        print(self.user)
+        print(self.password)
 
         self._open_sql_connection()
 
---
+++ db_scripts/migrate_1.sql
@@ -0,0 +1,42 @@
+USE atelier_bd
+
+ALTER TABLE singer RENAME TO musician
+
+ALTER TABLE musician RENAME COLUMN singerName TO musicianName
+
+ALTER TABLE musician ADD COLUMN role VARCHAR(50);
+
+ALTER TABLE musician ADD COLUMN bandName VARCHAR(50);
+
+UPDATE musician
+SET role = "vocals"
+WHERE musicianName = "Alina";
+
+UPDATE musician
+SET role = "guitar"
+WHERE musicianName = "Mysterio";
+
+UPDATE musician
+SET role = "percussion"
+WHERE musicianName = "Rainbow";
+
+UPDATE musician
+SET role = "piano"
+WHERE musicianName = "Luna";
+
+UPDATE musician
+SET bandName = "Crazy Duo"
+WHERE musicianName = "Alina" or musicianName = "Rainbow";
+
+UPDATE musician
+SET bandName = "Mysterio"
+WHERE musicianName = "Mysterio";
+
+UPDATE musician
+SET bandName = "Luna"
+WHERE musicianName = "Luna";
+
+CREATE TABLE band (bandName varchar(50) PRIMARY KEY, creation YEAR, genre varchar(50))
+
+INSERT INTO band
+VALUES ("Crazy Duo", 2015, "rock"), ("Luna", 2009, "classical"), ("Mysterio", 2019, "pop");
